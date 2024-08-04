a8c3a034c74a1593e66d4e58a959f822c0562a9a
Farrely-F <farrely.firenza@outlook.com>
2024-08-04 15:14:10 +0700

    feat: change on layout adding metadata

--- app/(shared)/share/[slug]/page.tsx
+++ app/(shared)/share/[slug]/page.tsx
@@ -1,8 +1,20 @@
 import { getPageBySlugWithPageLinks } from "@/lib/api/pages/queries";
 import { getUserAuth } from "@/lib/auth/utils";
-import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { page } = await getPageBySlugWithPageLinks(params.slug);
+  if (page === undefined) notFound();
+  return {
+    title: page.name,
+    description: page.description,
+  };
+}
 
 export default async function SharedPage({
   params,
@@ -15,7 +27,7 @@ export default async function SharedPage({
   if (page.public === false) return <main>This page is not public</main>;
   return (
     <main>
-      <div className="flex flex-col bg-[#708238] h-screen items-center justify-center py-8 px-4 text-center">
+      <div className="flex flex-col bg-gradient-to-b from-slate-900 to-slate-700 h-screen items-center justify-center py-8 px-4 text-center">
         <header className="mb-10">
           <div className="flex justify-center">
             <div className="w-24 h-24 rounded-full bg-gray-300">
@@ -29,11 +41,10 @@ export default async function SharedPage({
           <h1 className="text-2xl font-bold mt-4 text-white">{page.name}</h1>
           <p className="text-white">{page.description}</p>
         </header>
-        <nav className="flex-1 w-full max-w-md flex flex-col gap-4">
+        <nav className="flex-1 w-full max-w-xl flex flex-col gap-4">
           {pageLinks.map((l) => (
             <Link key={l.id} href={l.url}>
-              <div className="flex items-center gap-4 p-4 rounded-lg border border-gray-300 bg-white hover:bg-gray-200 transition-all duration-300">
-                <HomeIcon className="text-gray-500 w-5 h-5" />
+              <div className="flex items-center gap-4 p-4 rounded-lg border border-gray-300 bg-white hover:bg-gray-200 transition-all duration-300 hover:scale-105 ease-out">
                 <span className="text-gray-800">{l.title}</span>
               </div>
             </Link>
